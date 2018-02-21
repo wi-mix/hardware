7949c9482964fe7f39ae56c826dabd4ed35507c8
Dannick Pomerleau <dannickp@hotmail.com>
2018-02-21 13:25:34 -0700

    Working Timer

--- custom_modules/timer_tb.vhd
+++ custom_modules/timer_tb.vhd
@@ -6,27 +6,39 @@ entity timer_tb is
 end entity timer_tb;
 
 architecture test_bench of timer_tb is
-	component freq_divider is
+	component timer is
 	port (
 		clk		: in	std_logic;
+		enable	: in	std_logic;
 		target	: in	unsigned(31 downto 0);
-		done	: out	std_logic);
-	end component freq_divider;
+		done	: out	std_logic
+	);
+	end component timer;
 	
 	signal clk 		: std_logic := '0';
+	signal enable	: std_logic := '0';
 	signal target 	: unsigned(31 downto 0);
 	signal done 	: std_logic;
 begin
-	clk <= not clk after 2ns;
+	clk <= not clk after 1ns;
 	
 	stimulus : process
 	begin
+		wait for 0.5 ns;
 		target <= to_unsigned(2, 32);
+		wait for 1 ns;
+		enable <= '1';
 		wait for 10 ns;
+		enable <= '0';
+		wait for 1 ns;
 		target <= to_unsigned(4, 32);
+		wait for 1 ns;
+		enable <= '1';
+		wait for 20 ns;
+		enable <= '0';
 		wait;
 	end process stimulus;
 
 	
-	T0: freq_divider port map (clk => clk, target => target, done => done);
+	T0: timer port map (clk => clk, target => target, done => done, enable => enable);
 end architecture test_bench;
